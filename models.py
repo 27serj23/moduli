4460724f405402058720412e15a3ee4c163a40b3
27serj23 <154461692+27serj23@users.noreply.github.com>
2024-05-29 23:10:00 +0300

    Add files via upload

--- models.py
+++ models.py
@@ -1,19 +1,41 @@
 from django.db import models
 
 
-# Create your models here.
-class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
+class Category(models.Model):
+    name = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.title
+        return self.name
 
 
-class Comment(models.Model):
-    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
-    author = models.CharField(max_length=100)
-    content = models.TextField()
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/')
 
     def __str__(self):
-        return self.author
+        return self.name
+
+
+class Order(models.Model):
+    customer_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    address = models.TextField()
+    phone = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.customer_name
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.product.name
