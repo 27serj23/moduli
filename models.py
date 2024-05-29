a1f4e4f3c490af053404579084d4c648bedecb04
27serj23 <154461692+27serj23@users.noreply.github.com>
2024-05-29 23:00:15 +0300

    Add files via upload

--- models.py
+++ models.py
@@ -1,19 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-# Create your models here.
-class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-
-    def __str__(self):
-        return self.title
-
-
-class Comment(models.Model):
-    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
-    author = models.CharField(max_length=100)
-    content = models.TextField()
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    friends = models.ManyToManyField('self', symmetrical=False, blank=True)
+    notifications = models.ManyToManyField('Notification', blank=True)
 
     def __str__(self):
-        return self.author
+        return self.user.username
+
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.CharField(max_length=255)
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.message
